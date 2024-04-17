dd8882f9e49c61e309b8fe7284fbe744c73694bc
Stanisalv Terentyev <terentyev@intaro.email>
2024-04-17 18:53:03 +0400

    1.5.1

---
+++ sdk/Sources/PaymentForm/SafariViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SafariViewController.swift
+//  Cloudpayments
+//
+//  Created by CloudPayments on 17.05.2023.
+//
+
+import SafariServices
+import Foundation
+
+final class SafariViewController: SFSafariViewController {
+    
+}

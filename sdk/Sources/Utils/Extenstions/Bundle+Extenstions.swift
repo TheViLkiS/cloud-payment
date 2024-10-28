867aaeee3536293fe8ced0ff6494b69ad3f97afe
Dmitrii <110171206+TheViLkiS@users.noreply.github.com>
2024-10-28 15:59:42 +0400

    Update Bundle+Extenstions.swift

--- sdk/Sources/Utils/Extenstions/Bundle+Extenstions.swift
+++ sdk/Sources/Utils/Extenstions/Bundle+Extenstions.swift
@@ -9,36 +9,9 @@
 import Foundation
 import UIKit
 
-import Foundation
-import UIKit
-
 extension Bundle {
-    
     class var mainSdk: Bundle {
-        
-        if let sdkBundle = Bundle(identifier: "org.cocoapods.Cloudpayments"),
-           let bundleUrl = sdkBundle.url(forResource: "CloudpaymentsSDK", withExtension: "bundle"),
-           let resourceBundle = Bundle(url: bundleUrl) {
-            return resourceBundle
-        }
-        
-        let bundle = Bundle(for: PaymentForm.self)
-        if let bundleUrl = bundle.url(forResource: "CloudpaymentsSDK", withExtension: "bundle"),
-           let resourceBundle = Bundle(url: bundleUrl) {
-            return resourceBundle
-        }
-        
-        if let bundleUrl = Bundle.main.url(forResource: "CloudpaymentsSDK", withExtension: "bundle"),
-           let resourceBundle = Bundle(url: bundleUrl) {
-            return resourceBundle
-        }
-        
-        print("CloudpaymentsSDK.bundle not found")
-        return Bundle.main
-    }
-    
-    class var cocoapods: Bundle? {
-        return Bundle(identifier: "org.cocoapods.Cloudpayments")
+        return Bundle(for: PaymentForm.self)
     }
 }
 

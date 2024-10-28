5f2ccfc1f4e7199738dbe0e0adf080d57b13520c
Dmitrii <110171206+TheViLkiS@users.noreply.github.com>
2024-10-28 20:40:26 +0400

    Update Bundle+Extenstions.swift

--- sdk/Sources/Utils/Extenstions/Bundle+Extenstions.swift
+++ sdk/Sources/Utils/Extenstions/Bundle+Extenstions.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 
 extension Bundle {
-    
     class var mainSdk: Bundle {
         let bundle = Bundle.init(for: PaymentForm.self)
         let bundleUrl = bundle.url(forResource: "CloudpaymentsSDK", withExtension: "bundle")
@@ -20,7 +19,6 @@ extension Bundle {
         return Bundle(identifier: "org.cocoapods.Cloudpayments")
     }
 }
-
 
 
 
